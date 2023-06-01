634601897ad207177e50d28438dddc5ae7ce5eab
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2023-06-01 11:04:29 +0530

    Added data integration for zomato and todo dom app

---
+++ Day12/network.js
@@ -0,0 +1,91 @@
+function renderCategoryCard(data = []) {
+  const cardsArray = [];
+  const categoriesSliderContainer =
+    document.getElementById("categories-slider");
+  if (data.length > 0) {
+    data.forEach((_d) => {
+      const categoryCard = document.createElement("div");
+      categoryCard.setAttribute("class", "categories-card");
+      categoryCard.innerHTML = `<div class="card-content">
+                <img
+                  class="categories-image mb-1"
+                  src="${_d.image}"
+                />
+                <p class="text-center">${_d.label}</p>
+              </div>`;
+      cardsArray.push(categoryCard);
+    });
+  }
+  categoriesSliderContainer.append(...cardsArray);
+}
+
+function renderRestaurantCard(data = []) {
+  const cardsArray = [];
+  const restaurantsListingContainer = document.getElementById(
+    "restaurants-listing"
+  );
+  if (data.length > 0) {
+    data.forEach((_d) => {
+      const cardNode = document.createElement("div");
+      cardNode.setAttribute("class", "col-4");
+      cardNode.innerHTML = `
+            <div class="card restaurant-card p-1">
+              <img
+                src="${_d.restaurantImage}"
+                class="card-img-top"
+                height="248px"
+                style="border-radius: 10px; object-fit: cover"
+                alt="${_d.restaurantName}"
+              />
+              <div class="card-body">
+                <div class="row g-0 mb-2">
+                 <div class="col-10"><h6 class="restaurantTitle fs-8">${_d.restaurantName}</h6></div>
+                  <div class="col-2 ">
+                      <span class="badge text-bg-success restaurantRatings d-flex justify-content-between"><p class="fs-7">${_d.ratings}</p>
+                        <span class="material-symbols-outlined fs-7">
+                          star
+                        </span>
+                      </span>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-between">
+                  <div class="col-8">
+                    <p class="restaurantCuisine fs-7 fw-light">${_d.cuisine}</p>
+                  </div>
+                  <div class="col-4">
+                    <p class="restaurantCuisine text-end fs-7 fw-light">${_d.minQuanitityPrice}</p>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+      cardsArray.push(cardNode);
+    });
+    restaurantsListingContainer.append(...cardsArray);
+  }
+}
+
+function renderCategoriesData() {
+  const result = JSON.parse(this.responseText);
+  renderCategoryCard(result.data);
+}
+
+function renderRestaurantsData() {
+  const result = JSON.parse(this.responseText);
+  renderRestaurantCard(result.data);
+}
+
+function getCategories() {
+  var request = new XMLHttpRequest();
+  request.addEventListener("load", renderCategoriesData);
+  request.open("GET", "./categories.json");
+  request.send();
+}
+
+function getRestaurants() {
+  var request = new XMLHttpRequest();
+  request.addEventListener("load", renderRestaurantsData);
+  request.open("GET", "./restaurants.json");
+  request.send();
+}
+getCategories();
+getRestaurants();
